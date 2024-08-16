cadae3212b157c2e8566440af09c665811975764
Samuel Thomaz Bastos <samuel-tb@hotmail.com>
2024-08-15 23:37:51 -0300

    added function to get general results in utils_files.py

--- utils_file.py
+++ utils_file.py
@@ -3,16 +3,50 @@ import random
 import tensorflow as tf
 import numpy as np
 from tqdm import tqdm
-
+from numba import cuda 
 from input.reading_datasets import read_dataset_from_file
 from preprocessing.get_dummies_labels import GetDummiesLabels
 from preprocessing.train_test_split_module import TrainTestSplit
+import pickle
+import pandas as pd
 
 def set_seeds(seed):
     os.environ['PYTHONHASHSEED'] = str(seed)
     random.seed(seed)
     tf.random.set_seed(seed)
     np.random.seed(seed)
+
+def get_all_results():
+    all_dirs = os.listdir('./model_checkpoints')
+
+    info = []
+    for directory in all_dirs:
+        execution_info = {
+            'directory': directory,
+            'model_name': directory.split('_')[0],
+            'dataset': directory.split('_')[1],
+            'seed': directory.split('_')[2]
+        }
+
+        info.append(execution_info)
+
+        if 'model_history.pkl' in os.listdir('./model_checkpoints/' + directory):
+            with open('./model_checkpoints/' + directory + '/model_history.pkl', 'rb') as f:
+                history = pickle.load(f) 
+
+            execution_info['max_accuracy'] = max(history.history['val_accuracy'])
+            # execution_info['max_val_f1_score'] = max(history.history['val_f1_score'])
+            execution_info['min_val_loss'] = min(history.history['val_loss'])
+            execution_info['epochs'] = len(history.history['val_loss'])
+        else:
+            execution_info['max_accuracy'] = None
+            # execution_info['max_val_f1_score'] = None
+            execution_info['min_val_loss'] = None
+            execution_info['epochs'] = None
+
+    complete_data = pd.DataFrame(info)
+        
+    return complete_data
 
 def closest_power_of_2(n):
     if n < 1:
@@ -69,4 +103,5 @@ def training_nn_for_seeds(used_model, datasets = [], seeds = []):
                 pass
             else:
                 model.training_process()
-                print(model.model.evaluate(X_test, y_test))
+                device = cuda.get_current_device()
+                device.reset()
